6b197aee2fc3b6dc266e4a34856844b9c53adb36
zeyadamr8 <96020833+ZeyadAmr88@users.noreply.github.com>
2024-07-18 15:04:08 +0300

    Fix an error in Admin Home Page
    change name of export name from Home to AdminHome in Home.jsx in Admin folder , there is another file named Home.jsx in Home folder

--- src/Pages/Admin/components/Dashboard/components/Home/Home.jsx
+++ src/Pages/Admin/components/Dashboard/components/Home/Home.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import "./style/home.css";
 import { Outlet } from "react-router";
-export const Home = () => {
-  return (
-    
-      <Outlet />
-    
-  );
+export const AdminHome = () => {
+  return <Outlet />;
 };
