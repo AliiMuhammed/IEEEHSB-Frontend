1440cbc660df5c67280e6d99c0d98a39fdf57aa5
AliiMuhammed <mohamedtaha8000@gmail.com>
2024-07-23 21:39:21 +0300

    finish officers section in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,16 @@ import Values from "./Components/Values";
 import Chapters from "./Components/Chapters/Chapters";
 import Achievement from "./Components/Achievement/Achievement";
 import Hero from "./Components/Hero";
-import "./Style/home.css"
+import "./Style/home.css";
+import Officers from "./Components/Officers";
 const Home = () => {
   return (
     <section className="home-section">
       <Hero />
+      <Achievement />
+      <Officers />
       <Values />
       <Chapters />
-      <Achievement />
     </section>
   );
 };
